faf9a0115c174694fbd59870d63e200abd29c251
AbsoluteBedRest <63246653+AbsoluteBedRest@users.noreply.github.com>
2025-07-11 11:08:47 +0900

    needs(1)

--- needs/examples/example_wav2vec/unsupervised/scripts/ltr_to_wrd.py
+++ needs/examples/example_wav2vec/unsupervised/scripts/ltr_to_wrd.py
@@ -3,12 +3,14 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-"""
-Legacy entry point. Use fairseq_cli/train.py or fairseq-train instead.
-"""
 
-from fairseq_cli.train import cli_main
+import sys
+
+
+def main():
+    for line in sys.stdin:
+        print(line.replace(" ", "").replace("|", " ").strip())
 
 
 if __name__ == "__main__":
-    cli_main()
+    main()
